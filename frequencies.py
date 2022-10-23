ddabfe01fdfd3b3b315e214a4880c7558994f3a6
Qadir Ijaz Fernandez <qadir.ijaz@gmail.com>
2022-10-23 14:46:10 +0100

    Test solution

--- frequencies.py
+++ frequencies.py
@@ -5,7 +5,7 @@ def frequencies(items):
     frequencies = {}
     # Your code goes here
     for x in items:
-    #convert to string
+    #converting to string
         key = str(x)
         #adding new key and initialising to 0, increment run after if statement
         if key not in frequencies:
